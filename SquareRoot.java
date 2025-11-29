b6a24d0b23427d991ee3b5086723e35a657e9f5f
ismat-samadov <ismetsemedov@gmail.com>
2025-11-29 16:32:59 +0400

    init

--- SquareRoot.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class SquareRoot {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        long n = scanner.nextLong();
-        System.out.printf("%.6f", Math.sqrt(n));
-    }
-}
--- helloPy.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class helloPy {
-
-    public static void main(String[] args) {
-
-        System.out.println("Hello, Python!");
-    }
-}
